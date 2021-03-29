8c7ef646f981dc34f577e7962c000b4d94937f5b
Isac X <isacssw@gmail.com>
2021-03-29 13:09:33 +0100

    middleware to check if category exists

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -8,9 +8,21 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (req, res) => {
   const { name, description } = req.body;
 
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists) {
+    return res.status(400).json({ error: "Category Already exists!" });
+  }
+
   categoriesRepository.create({ name, description });
 
   return res.status(201).send();
+});
+
+categoriesRoutes.get("/", (req, res) => {
+  const all = categoriesRepository.list();
+
+  return res.json(all);
 });
 
 export { categoriesRoutes };
